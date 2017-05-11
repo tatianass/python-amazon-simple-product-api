2ef89691fa4398ab161485ff74da35b05413f6ad
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2017-05-11 18:13:12 -0300

    getting amazon link

--- examples/search_book_by_isbn.py
+++ examples/search_book_by_isbn.py
@@ -1,8 +1,15 @@
 import csv
+import json
 from amazon.api import AmazonAPI
+from pprint import pprint
+
+def writeToJSONFile(path, fileName, data):
+    filePathNameWExt = './' + path + '/' + fileName + '.json'
+    with open(filePathNameWExt, 'w') as fp:
+        json.dump(data, fp)
 
 keys = []
-with open('../rootkey.csv', 'rb') as keysfile:
+with open('../rootkey.csv', 'r') as keysfile:
 	spamreader = csv.reader(keysfile, delimiter=' ', quotechar='|')
 	for row in spamreader:
 		keys.extend(row)
@@ -12,5 +19,37 @@ AMAZON_SECRET_KEY = keys[1]
 AMAZON_ASSOC_TAG = keys[2]
 
 amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
-book = amazon.lookup(ItemId='1602820120', IdType='ISBN', SearchIndex='Books')
-print book
+
+#to json
+data = []
+with open('ff-books.json') as data_file:    
+    ff_books = json.load(data_file)
+    for book in ff_books:
+        if(book['isbn']):
+                isbn = book['isbn']
+                print(isbn)
+                try:
+                        item = amazon.lookup(ItemId=isbn, IdType='ISBN', SearchIndex='Books')
+
+                        item_link = ''
+                        if(type(item) is list):
+                                item_link = item[0].detail_page_url
+                        else:
+                                item_link = item.detail_page_url
+                                
+                        i = {}
+                        i['isbn'] = book.isbn
+                        i['isbn13'] = book.isbn13
+                        i['title'] = book.title
+                        i['author'] = book.authors[0].name
+                        i['link'] = book.link
+                        i['amazon-link'] = item_link
+                        data.append(i)
+                except Exception:
+                        print(Exception)
+                        ff_books.remove(book)
+                        pass
+                
+writeToJSONFile('./','amazon',data)
+
+
